4ced26790d2f4b01f44d289c752f292cc807d3b5
Kazi Robin <110531952+Kazirobin@users.noreply.github.com>
2024-09-16 16:48:02 +0600

    ok

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React,{ useState } from 'react'
+import './App.css'
+import Blogs from './components/Blogs'
+import From from './components/From'
+import Blog from './components/Blog'
+
+function App() {
+
+  return (
+    <>
+     <h1 className='m-auto block text-center text-2xl font-bold capitalize'>hello</h1>
+     <Blogs/>
+   
+     {/* <From/> */}
+    </>
+  )
+}
+
+export default App
